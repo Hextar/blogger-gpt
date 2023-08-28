8d304602221cd651e9437101a253825b7470ee9c
Stefano Salidu <st.salidu@gmail.com>
2023-08-28 16:41:17 +0200

    chore: added flake8 implementation on pre-commit

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from utils.print import print_colored
 from utils.template import get_user_name, get_chatgpt_name
 from utils.terminate import check_termination_condition
 
+
 async def main():
     print(f"Starting template {get_template()}\n\n")
 
@@ -16,8 +17,11 @@ async def main():
         user_message = await record_and_transcribe()
 
         # Not blocking the UX if the user message being empty
-        if (user_message == None):
-            print_colored(get_name(), "I didn't got your last phrase, can you please repeat?\n\n")
+        if (user_message is None):
+            print_colored(
+                get_user_name(),
+                "I didn't got your last phrase, can you please repeat?\n\n"
+            )
             continue
 
         # If a terminal condition is encountered exit immediately
@@ -32,9 +36,14 @@ async def main():
         print_colored(get_user_name(), f"{user_message}\n\n")
         print_colored(get_chatgpt_name(), f"{response}\n\n")
 
-        # Make sure no to include prepended text that Chat GPT sometime automatically adds
-        user_message_without_generate_image = re.sub(r'(Response:|Narration:|Image: generate_image:.*|)', '', response).strip()
-        
+        # Make sure no to include prepended text that Chat GPT
+        # sometime automatically adds
+        user_message_without_generate_image = re.sub(
+            r'(Response:|Narration:|Image: generate_image:.*|)',
+            '',
+            response
+        ).strip()
+
         # Sends to the eleven labs model Chat GPT response and play it
         text_to_speech(user_message_without_generate_image)
 
